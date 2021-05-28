69a90224c6951c21e1f2e726cc080639e5aefad4
moseffect21 <moseffect21@gmail.com>
2021-05-28 15:03:25 +0300

    change logo date

--- resources/js/components/pages/MainPage/components/FirstBlock.tsx
+++ resources/js/components/pages/MainPage/components/FirstBlock.tsx
@@ -7,7 +7,7 @@ const FirstBlock = () => {
     <div className={s.container}>
       {/* <img src="/images/image1.png" alt="" className={s.img1} />
       <img src="/images/image2.png" alt="" className={s.img2} /> */}
-      <img src="/images/logo.png" alt="" className={s.logo} />
+      <img src="/images/logo2.png" alt="" className={s.logo} />
     </div>
   )
 }
